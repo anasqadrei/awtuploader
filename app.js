98b75a96ba384577bc2f0228b566d38026bc6304
Anas Qadrei <anasqadrei@gmail.com>
2015-02-19 17:08:59 +1100

    read files in series

--- app.js
+++ app.js
@@ -1,3 +1,54 @@
 var fs = require('fs');
 
+var SOURCE_DIR = '../music_here';
+
+function uploadFile(filename, cb){
+  console.log(filename);
+  return cb();
+}
+
+function processFile(file, filesList, cb){
+  if (file) {
+    fs.stat(SOURCE_DIR + '/' + file, function(err, stats){
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+      else {
+        if (stats.isFile()) {
+          uploadFile(file, function(){
+            return processFile(filesList.shift(), filesList, function(){
+              return cb();
+            });
+          });
+        }
+        else {
+          return cb();
+        }
+      }
+    });
+  }
+  else {
+    return cb();
+  }
+}
+
+function listFiles(cb){
+  fs.readdir(SOURCE_DIR, function(err, files){
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    else {
+      processFile(files.shift(), files, function(){
+        return cb();
+      });
+    }
+  });
+}
+
 console.log('starting ...');
+
+listFiles(function(){
+  console.log('done');
+});
