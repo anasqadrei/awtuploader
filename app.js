e40495aa3172f7bcfbbe6bdfd2c22e7751fe11f2
Anas Qadrei <anasqadrei@gmail.com>
2015-02-21 00:24:28 +1100

    read file metadata from file name

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 var fs = require('fs');
 
 var SOURCE_DIR = '../music_here';
+var UPLOADER_USER_ID = 9;   //ya lail ya ain
 
-function uploadFile(filename, cb){
-  console.log(filename);
+function uploadFile(filePath, cb){
+  //[artist name]-[song title]-[genre]-[album]-[description]-[year]-[filename].ext
+  var fileElements = filePath.match(/(.+)(\/\[)(.+)(\]-\[)(.+)(\]-\[)(.*)(\]-\[)(.*)(\]-\[)(.*)(\]-\[)(.*)(\]-\[)(.+)(\])(\.mp3)$/);
+  var args = {
+    file: filePath,
+    songTitle: fileElements[5],
+    artistName: fileElements[3],
+    desc: fileElements[11],
+    userId: UPLOADER_USER_ID
+  }
+  //album
+  if (fileElements[9]) {
+    args.desc += '. ' + fileElements[9];
+  }
+  //genre
+  if (fileElements[7]) {
+    args.desc += ' #' + fileElements[7];    //check if # align with arabic
+  }
+  //year
+  if (fileElements[13]) {
+    args.desc += ' #' + fileElements[13];   //check if # align with arabic
+  }
+
+  console.log(args);
   return cb();
 }
 
@@ -16,7 +39,7 @@ function processFile(file, filesList, cb){
       }
       else {
         if (stats.isFile()) {
-          uploadFile(file, function(){
+          uploadFile(SOURCE_DIR + '/' + file, function(){
             return processFile(filesList.shift(), filesList, function(){
               return cb();
             });
