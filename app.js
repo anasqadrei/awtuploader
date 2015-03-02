55db49eba875696fb0f577642395ff224f8d7163
Anas Qadrei <anasqadrei@gmail.com>
2015-03-03 09:22:13 +1100

    upload to aws

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var fs = require('fs');
 var ffmpeg = require('fluent-ffmpeg');
+var aws = require('aws-sdk');
 var MongoClient = require('mongodb').MongoClient;
 var DB;
 var SOURCE_DIR = '../music_here';
 var UPLOADER_USER_ID = 9;   //ya lail ya ain
+var FILE_SUFFIX = '.new.mp3';
+
+aws.config = new aws.Config({
+  accessKeyId: process.env.AWS_USER_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_USER_SECRET_ACCESS_KEY
+});
+
+var s3 = new aws.S3();
 
 function getMaxId(collection, cb){
   DB.collection(collection).findOne({}, { _id: true }, { sort: [[ '_id', -1 ]] }, function(err, doc){
@@ -79,7 +88,6 @@ function readMetadata(args, cb){
     for (var i = 0; hashtags && i < hashtags.length; i++) {
       hashtags[i] = hashtags[i].replace(/#/g, '');
     }
-    console.log(hashtags);
 
     //insert artist
     findOrInsertArtist(args.artistName, function(err, artistId){
@@ -88,11 +96,86 @@ function readMetadata(args, cb){
         return cb(err);
       }
       else {
-        console.log(artistId);
+        //modify file metadata
+        ffmpeg(args.file)
+        .outputOptions('-codec copy')
+        .outputOptions('-metadata', 'title=' + args.songTitle)
+        .outputOptions('-metadata', 'artist=' + args.artistName)
+        .outputOptions('-metadata', 'comment=' + args.desc)        //doesn't update metadata!
+        .outputOptions('-metadata', 'genre=أوتاريكا')
+        .save(args.file + FILE_SUFFIX)
+        .on('error', function(err, stdout, stderr) {
+          return cb(err);
+        })
+        .on('end', function() {
+          //insert song
+          getMaxId('songs', function(err, newId){
+            if (err) {
+              return cb(err);
+            }
+            else {
+              var date = new Date();
+              var newSong = {
+                _id: newId,
+                title: args.songTitle,
+                artist: artistId,
+                desc: args.desc,
+                tags: hashtags,
+                uploader: UPLOADER_USER_ID,
+                fileSize: metadata.format.size,
+                duration: metadata.streams[metadataIndex].duration.toFixed(3) * 1000,
+                fileType: metadata.streams[metadataIndex].codec_name.trim().toUpperCase(),
+                sampleRate: metadata.streams[metadataIndex].sample_rate,
+                bitrate: metadata.streams[metadataIndex].bit_rate,
+                createdDate: date.toISOString(),
+                commentsCount: 0,
+                playsCount: 0,
+                listenersCount: 0,
+                downloadsCount: 0,
+                likesCount: 0,
+                dislikesCount: 0
+              };
 
-        //insert song
+              DB.collection('songs').insert(newSong, function(err, doc){
+                if (err) {
+                  return cb(err);
+                }
+                else {
+                  //upload to S3
+                  var stream = fs.createReadStream(args.file + FILE_SUFFIX);
+                  var params = {
+                    Bucket: process.env.AWS_S3_BUCKET_MUSIC,
+                    Key: 'songs/original/' + Math.floor(newId/1000) + '/' + newId + '.mp3',
+                    Body: stream,
+                    ContentType: 'audio/mp3',
+                  };
+                  s3.putObject(params, function(err, data) {
+                    if (err){
+                      return cb(err);
+                    }
+                    else{
+                      //delete the newly created file by ffmpeg
+                      fs.unlink(args.file + FILE_SUFFIX, function(err){
+                        if(err){
+                          console.log(err);
+                          return cb(err);
+                        }
+                        else {
+                          //update statistcs
+                          DB.collection('artists').update({ _id: artistId }, { $inc: { songsCount: 1 } }, { w: 0 });
+                          DB.collection('usersong').update({ user: UPLOADER_USER_ID, song: newId }, { $set: { upload: true } }, { upsert: true, w: 0 });
 
-        return cb();
+                          //final return
+                          return cb();
+                        }
+                      });
+                    }
+                  });
+                }
+              });
+            }
+          });
+        });
       }
     });
   });
@@ -104,8 +187,8 @@ function uploadFile(filePath, cb){
   var fileElements = filePath.match(/(.+)(\/\[)(.+)(\]-\[)(.+)(\]-\[)(.*)(\]-\[)(.*)(\]-\[)(.*)(\]-\[)(.*)(\]-\[)(.+)(\])(\.mp3)$/);
   var args = {
     file: filePath,
-    songTitle: fileElements[5],
-    artistName: fileElements[3].replace('+', ' ').trim(),
+    songTitle: fileElements[5].replace(/(;|#|&|@|:|=|`|>|<|%|~|\"|\'|\/|\\|\||\{|\}|\(|\)|\[|\]|\+|\*|\$|\?|\^)/g, ' ').trim(),
+    artistName: fileElements[3].replace(/(;|#|&|@|:|=|`|>|<|%|~|\"|\'|\/|\\|\||\{|\}|\(|\)|\[|\]|\+|\*|\$|\?|\^)/g, ' ').trim(),
     desc: fileElements[11],
     userId: UPLOADER_USER_ID
   }
@@ -115,12 +198,14 @@ function uploadFile(filePath, cb){
   }
   //genre
   if (fileElements[7]) {
-    args.desc += ' #' + fileElements[7];    //check if # align with arabic
+    args.desc += ' #' + fileElements[7].replace(/\s/g, '_');
   }
   //year
   if (fileElements[13]) {
-    args.desc += ' #' + fileElements[13];   //check if # align with arabic
+    args.desc += ' #' + fileElements[13].replace(/\s/g, '_');
   }
+  //song title and artist
+  args.desc += ' #' + args.songTitle.replace(/\s/g, '_') + ' #' + args.artistName.replace(/\s/g, '_');
 
   return readMetadata(args, function(){
     return cb();
@@ -168,7 +253,7 @@ function listFiles(cb){
 }
 
 console.log('starting ...');
-MongoClient.connect('mongodb://anas:anas1234@localhost:27017/awtphase2', function(err, db) {
+MongoClient.connect(process.env.MONGOHQ_URL, function(err, db) {
   if(err) {
     throw err;
   }
